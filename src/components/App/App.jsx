617fe19595eb8f289dd4afdf55d9cad84f46d79c
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-20 19:51:57 +0200

    add initial files & inst axios

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import { Component } from 'react';
 class App extends Component {
   state = {};
 
+  API_KEY = '39983017-ab6cfdfc6bbf03f7c61f72b59';
+
   componentDidMount() {}
 
   componentDidUpdate(prevProps, prevState) {}
