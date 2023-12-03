47582c5cf009849476d19d4cc8b45bb03b6c6e5e
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-03 22:59:32 +0200

    add load more btn component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import { LoadMoreBtn } from './Button.styled';
+
+const Button = ({ onLoadMore }) => {
+  return (
+    <LoadMoreBtn type="button" onClick={onLoadMore}>
+      Load more
+    </LoadMoreBtn>
+  );
+};
+
+export default Button;
